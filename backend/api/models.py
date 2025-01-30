9cdd1239c96ede6019ac4759b85b02ae339a0dfb
CassioAA <cassioalmeidaccti@gmail.com>
2025-01-30 16:51:54 -0300

    add: Routing for* notes interaction views

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User, # each note belongs to one user/author
+        on_delete=models.CASCADE, # user/author deleted, all notes deleted
+        related_name='notes', # instead of note_set default
+    )
+
+    def __str__(self):
+        return self.title
